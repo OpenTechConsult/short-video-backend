74f0e7d77558458d4d9fb54d6777fd2db466b325
OpenTech-Consult <opentech.consult@gmail.com>
2022-04-21 21:24:33 +0000

    MongoDB schema and routes
    - created a schema file required by Mongoose.
    - created a short video schema then created a short video model
    - used the schema and the model to create two API endpoints:
     * GET /v2/posts to get all the videos in the mongodb database
     * POST /v2/posts to add short videos data to the mongodb atlas database
    
    closes #4

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Cors from 'cors';
+import ShortVideo from './dbModel.js';
 
 // App config
 const app = express();
 const port = process.env.PORT || 9000;
-const connection_url = 'mongodb+srv://eagboka:azerty123@shortvideomerndb.ydf1p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const connection_url = 'mongodb+srv://eagboka:azerty123@shortvideomerndb.ydf1p.mongodb.net/shortvideodb?retryWrites=true&w=majority';
 
 // Middleware
-
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -15,8 +18,29 @@ mongoose.connect(connection_url, {
     useUnifiedTopology: true,
 });
 
-// Routes
+// API Endpoints - Routes
 app.get('/', (req, res) => res.status(200).send('Hello TheWebdev!'));
+
+app.post('/v2/posts', (req, res) => {
+    const dbVideos = req.body;
+    ShortVideo.create(dbVideos, (err, data) => {
+        if (err) { 
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+})
+
+app.get('/v2/posts', (req, res) => {
+    ShortVideo.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+})
 
 // Listener
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
